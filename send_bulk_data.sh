4560a209af79a660dcd76d5d00eda998c5dbdbcb
Jose Macchi <jose.macchi@gmail.com>
2022-03-15 14:27:17 -0300

    Grafana dashboard. Docs

--- send_bulk_data.sh
+++ send_bulk_data.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+# se requiere un parametro de entrada en script, que indica cantidad de ciclos/mensajes que se enviaran
 for (( c=1; c<=$1; c++ ))
 do
-	./send_data.sh "hi"+$c
+	./send_data.sh "test-data"+$c
 done
 
